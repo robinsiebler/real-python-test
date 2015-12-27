78f624fc9f0b6c6cb3a5a5168343684e9a6b7758
Robin Siebler <robinsiebler@dslextreme.com>
2015-12-27 12:41:50 -0800

    Changed from using SQLAlchemy to Flask-SQLAlchemy

--- flasktaskr/project/_config.py
+++ flasktaskr/project/_config.py
@@ -1,3 +1,5 @@
+# project/_config.py
+
 import os
 
 # grab the folder where this script lives
@@ -11,3 +13,6 @@ SECRET_KEY = '817DA6CF78F7D'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
